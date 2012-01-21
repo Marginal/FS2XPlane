0c1c09aff476090f9b4da644ce679f56ba12286d
Jonathan Harris <x-plane@marginal.org.uk>
2012-01-21 19:34:02 +0000

    Windows build fix (really). Updated installer icon.

--- dist.cmd
+++ dist.cmd
@@ -1,4 +1,7 @@
 @echo off
+
+for /f "usebackq tokens=1,2" %%I in (`python.exe -c "from version import appversion; print '%%4.2f %%d'%%(appversion, round(appversion*100,0))"`) do (set VERSION=%%I&set VER=%%J)
+
 if exist build rd /s /q build
 python -OO win32\setup.py -q py2exe
 "C:\Program Files\NSIS\makensis.exe" /nocd /v2 win32\FS2XPlane.nsi
